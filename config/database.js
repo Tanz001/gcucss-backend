b5dded02781d05b213771d11c7939560c28ec420
Tanz001 <chinopacer55@gmail.com>
2025-11-19 19:58:56 -0500

    Connection error resolved

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'mysql-11c18ca8-chinopacer55-48f5.e.aivencloud.com',
   user: process.env.DB_USER || 'avnadmin',
-  password: process.env.DB_PASSWORD || 'P@AVNS_g6yz-7YMmh2c4jl3dS1',
+  password: process.env.DB_PASSWORD || 'AVNS_g6yz-7YMmh2c4jl3dS1',
   database: process.env.DB_NAME || 'css',
   port: process.env.DB_PORT || 10718,
   waitForConnections: true,
