1400e74ddacab11eb31cdef0013a92ac072181b3
Tanz001 <chinopacer55@gmail.com>
2025-11-19 19:38:26 -0500

    database connection

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'P@kistan66',
+  host: process.env.DB_HOST || 'mysql-11c18ca8-chinopacer55-48f5.e.aivencloud.com',
+  user: process.env.DB_USER || 'avnadmin',
+  password: process.env.DB_PASSWORD || 'P@AVNS_g6yz-7YMmh2c4jl3dS1',
   database: process.env.DB_NAME || 'css',
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 10718,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
